698eefdf004e42568352896842218c1032335746
Chuck <chuck@chihai.me>
2023-02-15 17:44:14 +0800

    :tada: Eagle PAG文件格式扩展

---
+++ README.md
@@ -0,0 +1,16 @@
+# Eagle-Plugin-PAG-Preview-Core
+> Eagle PAG文件格式扩展
+
+## 开发
+### 安装依赖
+```shell
+yarn # npm i
+```
+### 打包编译
+```shell
+yarn run build # npm run build
+```
+### 发布
+```shell
+yarn run publ # npm run publ
+```
---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: "build/core.js",
+                format: "cjs"
+            },
+            {
+                file: "build/index.cjs",
+                format: "cjs"
+            },
+            {
+                file: "build/index.mjs",
+                format: "esm"
+            }
+        ],
+        plugins: [
+            resolve(),
+            typescript({
+                cacheRoot: "./node_modules/.cache/rollup-plugin-typescript2"
+            }),
+            commonjs(),
+            babel({
+                babelHelpers: "runtime",
+                exclude: "node_modules/**",
+                extensions: [".ts"],
+                presets: ["@babel/preset-env"],
+                plugins: ["@babel/plugin-transform-runtime"]
+            }),
+            terser()
+        ]
+    }
+];
